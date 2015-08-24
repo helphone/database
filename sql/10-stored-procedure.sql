65667062208f459617f730432ce6dc26289cb1fe
Gaël Gillard <gillardgael@gmail.com>
2015-08-24 16:38:32 +0200

    If a category translation is missing, fallback to english

--- sql/10-stored-procedure.sql
+++ sql/10-stored-procedure.sql
@@ -20,8 +20,13 @@ BEGIN
     FROM phone_numbers ph
       LEFT JOIN phone_numbers_to_phone_numbers_categories phcat ON ph.id = phcat.phone_number_id
       LEFT JOIN phone_numbers_categories cat ON phcat.phone_number_categories_name = cat.name
-      LEFT JOIN phone_numbers_categories_translations tr ON cat.name = tr.category_name AND tr.language_code =' || quote_literal(language_code) ||
-    'WHERE ph.country_code =' || quote_literal(country_code);
+      LEFT JOIN (
+        SELECT e.category_name, COALESCE(o.value,e.value) AS "value"
+        FROM phone_numbers_categories_translations              e
+          LEFT OUTER JOIN phone_numbers_categories_translations o ON e.category_name=o.category_name and o.language_code=' || quote_literal(language_code) || '
+        WHERE e.language_code='|| quote_literal('en') ||'
+      ) tr ON cat.name = tr.category_name
+    WHERE ph.country_code =' || quote_literal(country_code);
 
   RETURN QUERY EXECUTE sql;
 
