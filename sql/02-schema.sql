b495183617210cf32caee579730a14243984fe41
Gaël Gillard <gillardgael@gmail.com>
2015-10-23 13:58:51 +0200

    Add index to the schema and convert a unique constraint to primary key

--- sql/02-schema.sql
+++ sql/02-schema.sql
@@ -9,22 +9,26 @@ CREATE TABLE languages (
 );
 
 CREATE TABLE countries_translations (
-  country_code CHAR(2) REFERENCES countries (code),
-  language_code CHAR(2) REFERENCES languages (code),
+  country_code CHAR(2) REFERENCES countries (code) NOT NULL,
+  language_code CHAR(2) REFERENCES languages (code) NOT NULL,
   value VARCHAR(255) NOT NULL,
   CONSTRAINT PK_countries_translations PRIMARY KEY (country_code, language_code)
 );
+CREATE INDEX IN_countries_translations_country_code ON countries_translations USING betree (country_code);
+CREATE INDEX IN_countries_translations_language_code ON countries_translations USING betree (language_code);
 
 CREATE TABLE phone_numbers_categories (
   name VARCHAR(100) CONSTRAINT PK_phone_numbers_categories PRIMARY KEY
 );
 
 CREATE TABLE phone_numbers_categories_translations (
-  category_name VARCHAR(100) REFERENCES phone_numbers_categories (name),
-  language_code CHAR(2) REFERENCES languages (code),
+  category_name VARCHAR(100) REFERENCES phone_numbers_categories (name) NOT NULL,
+  language_code CHAR(2) REFERENCES languages (code) NOT NULL,
   value VARCHAR(255) NOT NULL,
   CONSTRAINT PK_phone_numbers_categories_translations PRIMARY KEY (category_name, language_code)
 );
+CREATE INDEX IN_phone_numbers_categories_translations_category_name ON phone_numbers_categories_translations USING betree (country_code);
+CREATE INDEX IN_phone_numbers_categories_translations_language_code ON phone_numbers_categories_translations USING betree (language_code);
 
 CREATE TABLE phone_numbers (
   id BIGSERIAL CONSTRAINT PK_phone_numbers PRIMARY KEY,
@@ -32,9 +36,12 @@ CREATE TABLE phone_numbers (
   phone_number VARCHAR(100) NOT NULL,
   CONSTRAINT UNIQUE_phone_numbers UNIQUE (country_code, phone_number)
 );
+CREATE INDEX IN_phone_numbers_country_code ON phone_numbers USING betree (country_code);
 
 CREATE TABLE phone_numbers_to_phone_numbers_categories (
   phone_number_id BIGINT REFERENCES phone_numbers (id) NOT NULL,
   phone_number_categories_name VARCHAR(100) REFERENCES phone_numbers_categories (name) NOT NULL,
-  CONSTRAINT UNIQUE_phone_numbers_to_phone_numbers_categories UNIQUE (phone_number_id, phone_number_categories_name)
+  CONSTRAINT PK_phone_numbers_to_phone_numbers_categories PRIMARY KEY (phone_number_id, phone_number_categories_name)
 );
+CREATE INDEX IN_phone_numbers_to_phone_numbers_categories_phone_number_id ON phone_numbers_to_phone_numbers_categories USING betree (phone_number_id);
+CREATE INDEX IN_phone_numbers_to_phone_numbers_categories_phone_number_categories_name ON phone_numbers_to_phone_numbers_categories USING betree (phone_number_categories_name);
