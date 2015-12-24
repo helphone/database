cf13e1e26f7ba91525ce9a5983b5bba6f058b617
Gaël Gillard <gillardgael@gmail.com>
2015-12-24 12:22:45 +0100

    Change the second stored procedure to get translations for countries
    The previous one could be simply replaced with a WHERE inside the SQL query

--- sql/06-stored-procedure_02.sql
+++ sql/06-stored-procedure_02.sql
@@ -1,33 +1,27 @@
 \c helphone
 
-CREATE OR REPLACE FUNCTION numbers_by_country_and_language(
-  input_country_code VARCHAR,
+CREATE OR REPLACE FUNCTION countries_by_language(
   input_language_code VARCHAR
 )
 RETURNS TABLE (
-  category_name VARCHAR,
-  phone_number VARCHAR,
+  country_name VARCHAR,
   country_code CHAR(2)
 )
 AS $body$
 DECLARE
-  input_country_code ALIAS FOR $1;
-  input_language_code ALIAS FOR $2;
+  input_language_code ALIAS FOR $1;
   sql VARCHAR;
 BEGIN
 
   sql := '
-    SELECT tr.value AS "category", ph.phone_number, ph.country_code AS "phonenumber"
-    FROM phone_numbers ph
-      LEFT JOIN phone_numbers_to_phone_numbers_categories phcat ON ph.id = phcat.phone_number_id
-      LEFT JOIN phone_numbers_categories cat ON phcat.phone_number_categories_name = cat.name
+    SELECT tr.value AS "country_name", countries.code as "country_code"
+    FROM countries
       LEFT JOIN (
-        SELECT e.category_name, COALESCE(o.value,e.value) AS "value"
-        FROM phone_numbers_categories_translations              e
-          LEFT OUTER JOIN phone_numbers_categories_translations o ON e.category_name=o.category_name and o.language_code=' || quote_literal(input_language_code) || '
+        SELECT e.country_code, COALESCE(o.value,e.value) AS "value"
+        FROM countries_translations              e
+          LEFT OUTER JOIN countries_translations o ON e.country_code=o.country_code and o.language_code=' || quote_literal(input_language_code) || '
         WHERE e.language_code='|| quote_literal('en') ||'
-      ) tr ON cat.name = tr.category_name
-    WHERE ph.country_code =' || quote_literal(input_country_code);
+      ) tr ON countries.code = tr.country_code';
 
   RETURN QUERY EXECUTE sql;
 
